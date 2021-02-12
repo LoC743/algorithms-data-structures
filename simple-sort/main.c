23e3bf84b563ae78ba41c2ad4a126a96bf4adea0
Alexey Morozov <alexey1562@gmail.com>
2021-02-12 14:50:23 +0300

    [Урок 3]: Реализовано задание 1

--- simple-sort/main.c
+++ simple-sort/main.c
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <time.h>
+
+void task1();
 
 int main() {
     char char_choice[3];
@@ -23,6 +27,7 @@ int main() {
 		switch (int_choice)
 		{
             case 1:
+            task1();
                 break;
             case 2:
                 break;
@@ -37,4 +42,102 @@ int main() {
 	} while(int_choice != exit_code);
 
     return 0;
+}
+
+int* generate_array(size_t size) {
+    if (size <= 0) { 
+        int* empty_array = (int*) calloc(0, sizeof(int));
+        return empty_array;
+    }
+
+    int* array = (int*) calloc(size, sizeof(int));
+    for (size_t i = 0; i < size; ++i) {
+        array[i] = rand() % 101;
+    }
+
+    return array;
+}
+
+int* create_copy_of_array(int* array, int size) {
+    int* copy = (int*) calloc(size, sizeof(int));
+    for (int i = 0; i < size; ++i) {
+        copy[i] = array[i];
+    }
+
+    return copy;
+}
+
+void print_array(int* array, int size) {
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", array[i]);
+    }
+}
+
+void swap(int* first, int* second) {
+    *first  = *first  ^ *second;
+    *second = *first  ^ *second;
+    *first  = *second ^ *first;
+}
+
+int bubble_sort(int* array, int size) {
+    int op_counter = 0;
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - 1; ++j) {
+            ++op_counter;
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+            }
+        }
+    }
+    return op_counter;
+}
+
+int bubble_sort_optimized(int* array, int size) {
+    int op_counter = 0;
+    for (size_t i = 0; i + 1 < size; ++i) {
+        for (size_t j = 0; j + 1 < size - i; ++j) {
+            ++op_counter;
+            if (array[j + 1] < array[j]) {
+                swap(&array[j], &array[j + 1]);
+            }
+        }
+    }
+    return op_counter;
+}
+
+void task1() {
+    int array_size = 0;
+
+    printf("\nВведите размер массива: ");
+    scanf("%d", &array_size);
+
+    int* array = generate_array(array_size);
+    int* array_copy = create_copy_of_array(array, array_size);
+    printf("Полученный массив: ");
+    // print_array(array, array_size);
+
+    // Сортировка пузырьком
+    clock_t begin_bubble = clock();
+    int bubble_op = bubble_sort(array, array_size);
+    clock_t end_bubble = clock();
+
+    printf("\n\n[Bubble sort]: ");
+    // print_array(array, array_size);
+    double time_spent_bubble = (double)(end_bubble - begin_bubble) / CLOCKS_PER_SEC;
+    printf("\nЗатраченное время: %lf", time_spent_bubble);
+    printf("\nКоличество операций: %d\n", bubble_op);
+
+    // Сортировка пузырьком с оптимизациями
+    clock_t begin_bubble_op = clock();
+    int bubble_optimized_op = bubble_sort_optimized(array_copy, array_size);
+    clock_t end_bubble_op = clock();
+
+    printf("\n[Bubble sort оптимизированный]: ");
+    // print_array(array_copy, array_size);
+    double time_spent_bubble_op = (double)(end_bubble_op - begin_bubble_op) / CLOCKS_PER_SEC;
+    printf("\nЗатраченное время: %lf", time_spent_bubble_op);
+    printf("\nКоличество операций: %d\n", bubble_optimized_op);
+
+    free(array);
+    free(array_copy);
 }
